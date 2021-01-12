e9ea191ec8b0eef591db2a7e2a5989663deb9384
W_gaijun <326384259@qq.com>
2021-01-12 18:41:38 +0800

    完成登录页

---
+++ src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+      <el-button type="info" @click="loginOut">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  components: {
+
+  },
+  data () {
+    return {
+
+    }
+  },
+  mounted () {
+
+  },
+  created () {
+
+  },
+  methods: {
+    loginOut () {
+      // 销毁token
+      window.sessionStorage.clear()
+      // 跳转回登录
+      this.$router.push('login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
